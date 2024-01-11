e2484962be1e33026030d0b5edb72e6f8dff553e
shivansh13579 <shivansh0975@gmail.com>
2024-01-11 21:45:17 +0530

    connect mongodb database

---
+++ app.js
@@ -0,0 +1,35 @@
+// const express = require('express');
+import express from 'express';
+import cors from 'cors'
+import cookieParser from "cookie-parser";
+import {config} from 'dotenv';
+import morgan from 'morgan';
+config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    credentials: true
+}));
+
+app.use(cookieParser());
+
+app.use(morgan('dev'));
+
+app.use('/ping',function(req,res){
+    res.send('/pong');
+});
+
+//route of three modules;
+
+
+app.all('*',(req,res)=>{
+    res.status(404).send('OOPS!! 404 page not found');
+});
+
+export default app;
+
+
