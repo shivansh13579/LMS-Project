281ea739b777db17d12cfa3a26bd0390c4bb6af7
shivansh13579 <shivansh0975@gmail.com>
2024-01-12 22:01:00 +0530

    create error middleware

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ import cors from 'cors'
 import cookieParser from "cookie-parser";
 import {config} from 'dotenv';
 import morgan from 'morgan';
-import userRoutes from "../server/routes/user.routes"
+import userRoutes from "./routes/user.routes.js";
+import errorMiddleware from './middlewares/error.middleware.js';
 config();
 
 const app = express();
@@ -31,6 +32,8 @@ app.use('/api/v1/user',userRoutes)
 app.all('*',(req,res)=>{
     res.status(404).send('OOPS!! 404 page not found');
 });
+
+app.use(errorMiddleware);
 
 export default app;
 
