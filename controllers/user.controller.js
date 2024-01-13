1a609e3c0308854510a9cfeef28064ca1a829777
shivansh13579 <shivansh0975@gmail.com>
2024-01-13 06:33:07 +0530

    create register user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,28 @@
-const register = (req,res) => {
-    
+import AppError from "../utils/error.util";
+import userSchema from "../models/user.model.js"
+
+const register = async (req,res,next) => {
+    const {fullName,email,password} = req.body;
+
+    if(!fullName || !email || !password){
+        return next(new AppError('All fields are required',400));
+    }
+
+    const userExists = await User
+
+    if(userExists){
+        return next(new AppError('Email already exists',400));
+    } 
+
+    const user = await User.create({
+        fullName,
+        email,
+        password,
+        avatar: {
+            public_id: email,
+            secure_url: 
+        }
+    })
 };
 
 const login = (req,res) => {
---
+++ models/user.model.js
@@ -0,0 +1,47 @@
+import { Schema,model } from "mongoose";
+
+const userSchema = new Schema({
+      fullName: {
+        type: 'String',
+        required: [true,'Name is required'],
+        minLength: [5,'Name must be at least 5 charachter'],
+        maxLength: [50,'Name should be less than 50 character'],
+        lowercase: true,
+        trim: true
+      },
+      email: {
+        type: 'String',
+        required: [true,'Email is required'],
+        lowercase: true,
+        trim: true,
+        unique: true,
+        match: [/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,'Please fill in a valid email address']
+      },
+      password: {
+        type: 'String',
+        required: [true,'Password is required'],
+        minLength: [8,'Password must be at least 8 character'],
+        select: false
+      }, 
+      avatar: {
+        public_id: {
+            type: 'String'
+        },
+        secure_url: {
+            type: 'String'
+        }
+      },
+      role: {
+        type: 'String',
+        enum: ['USER','ADMIN'],
+        default: 'USER'
+      },
+      forgotPasswordToken: String,
+      forgotPasswordExpiry: Date         
+},{
+    timestamps: true
+});
+
+const User = model('User',userSchema);
+
+export default User;
